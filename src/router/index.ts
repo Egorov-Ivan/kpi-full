f6476ec1de7f1c18ac9b2e04a6edbd555bc12b86
Egorov Ivan <egorov.ivan@example.com>
2026-05-06 19:31:03 +0300

    Исправлен парсинг чисел и формула KPI VAT

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+  path: '/',
+  redirect: '/kpi-calculator'
+    },
+    {
       path: '/',
       name: 'home',
       component: MainPage
